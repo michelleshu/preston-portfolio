656d1565c2695ddf651c9f1cdac614f8236efcb9
Michelle Shu <shu.michelle.w@gmail.com>
2025-06-27 15:32:12 -0600

    Add basic boilderplate

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "@radix-ui/themes/styles.css";
+import { Theme } from "@radix-ui/themes";
+
+import App from "./components/App";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <Theme appearance="dark">
+      <App />
+    </Theme>
+  </React.StrictMode>
+);
