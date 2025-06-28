1ca246cbc28ea905f6ce119fc321322e13fde25a
Michelle Shu <shu.michelle.w@gmail.com>
2025-06-28 16:45:58 -0600

    Support gh-pages

--- src/index.js
+++ src/index.js
@@ -1,15 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
 import "@radix-ui/themes/styles.css";
 import { Theme } from "@radix-ui/themes";
 
 import App from "./components/App";
 
+const router = createBrowserRouter([
+  {
+    path: "",
+    element: <App />,
+  },
+]);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Theme appearance="dark">
-      <App />
+      <RouterProvider router={router} />
     </Theme>
   </React.StrictMode>
 );
