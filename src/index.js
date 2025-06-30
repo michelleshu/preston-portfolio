5605ae2485de476d4900f35aed9063d503794438
Michelle Shu <shu.michelle.w@gmail.com>
2025-06-29 23:11:44 -0600

    Switch to hash router

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createHashRouter, RouterProvider } from "react-router-dom";
 
 import "@radix-ui/themes/styles.css";
 import { Theme } from "@radix-ui/themes";
@@ -8,7 +8,7 @@ import { Theme } from "@radix-ui/themes";
 import Home from "./routes/Home";
 import Error from "./routes/Error";
 
-const router = createBrowserRouter([
+const router = createHashRouter([
   {
     path: "/",
     element: <Home />,
